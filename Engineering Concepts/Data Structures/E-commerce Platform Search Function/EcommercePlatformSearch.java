191e2a94b43b576e48a2ae24915431717e61e5bf
Sanjai S <sanjais2205@gmail.com>
2025-06-19 17:23:36 +0530

    Update EcommercePlatformSearch.java

--- Engineering Concepts/Data Structures/E-commerce Platform Search Function/EcommercePlatformSearch.java
+++ Engineering Concepts/Data Structures/E-commerce Platform Search Function/EcommercePlatformSearch.java
@@ -5,15 +5,19 @@ public class EcommercePlatformSearch {
             new Product(102, "Shoes", "Footwear"),
             new Product(103, "Watch", "Accessories")
         };
+        System.out.println("This is Binary Search:");
         Product result = LinearAndBinarySearch.binarySearch(products, 102);
         if (result != null) {
             System.out.println("Found: " + result.productName);
         } else {
             System.out.println("Product not found");
         }
-         Product result2 = LinearAndBinarySearch.binarySearch(products, 109);
-        if (result2 != null) {
-            System.out.println("Found: " + result2.productName);
+        System.out.println();
+        System.out.println();
+        System.out.println("This is Linear search:");
+        Product result3=LinearAndBinarySearch.binarySearch(products, 101);
+        if (result3!= null) {
+            System.out.println("Found: " + result3.productName);
         } else {
             System.out.println("Product not found");
         }
