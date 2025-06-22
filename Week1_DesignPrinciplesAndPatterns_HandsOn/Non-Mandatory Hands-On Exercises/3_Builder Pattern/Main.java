f2c569c6a8dac4c001c6f10c99a68697e8e79519
Sanjai S <sanjais2205@gmail.com>
2025-06-22 16:11:44 +0530

    Add files via upload

---
+++ Week1_DesignPrinciplesAndPatterns_HandsOn/Non-Mandatory Hands-On Exercises/3_Builder Pattern/Main.java
@@ -0,0 +1,18 @@
+public class Main {
+    public static void main(String[] args) {
+        Computer gamingPC = new Computer.Builder()
+                .setCPU("Intel i9")
+                .setRAM("32GB")
+                .setStorage("1TB SSD")
+                .build();
+        Computer officePC = new Computer.Builder()
+                .setCPU("Intel i5")
+                .setRAM("8GB")
+                .setStorage("512GB SSD")
+                .build();
+        System.out.println("Gaming PC Configuration:");
+        gamingPC.display();
+        System.out.println("\nOffice PC Configuration:");
+        officePC.display();
+    }
+}
